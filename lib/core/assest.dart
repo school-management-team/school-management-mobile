29830a88b4ef744181f621b425dda83d3ae59aa0
reem <barroro511@gmail.com>
2026-05-13 17:38:24 +0300

    add image required by page slection role

--- lib/core/assest.dart
+++ lib/core/assest.dart
@@ -1,10 +1,13 @@
 class AssestData {
-
-      static const Logo="assest/images/Mobile Logo.png";
-     static const Horizontal_Divider="assest/images/Horizontal Divider.png";
-      static const circule="assest/images/cir.png";
-      static const circule1="assest/images/c1.png";
-      static const Mainlogo="assest/images/logoicon.png";
-       static const overlay="assest/images/Overlay.png";
-         static const loading="assest/images/Loading.json";
+  static const Logo = "assest/images/Mobile Logo.png";
+  static const Horizontal_Divider = "assest/images/Horizontal Divider.png";
+  static const circule = "assest/images/cir.png";
+  static const circule1 = "assest/images/c1.png";
+  static const Mainlogo = "assest/images/logoicon.png";
+  static const overlay = "assest/images/Overlay.png";
+  static const loading = "assest/images/Loading.json";
+  static const roleImage = "assest/images/Roleimage.png";
+  static const roleSystem = "assest/images/role2.png";
+  static const roleTeacher = "assest/images/role3.png";
+  static const roleStudent = "assest/images/role4.png";
 }
